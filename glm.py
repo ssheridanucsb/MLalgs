8ac2383165d7923d660046c53783462409c159ce
Sam Sheridan <samuelsheridan@ucsb.edu>
2020-03-26 13:18:16 -0700

    implemented regular linear regression

--- glm.py
+++ glm.py
@@ -1,24 +1,29 @@
 import numpy as np
 
 class LinReg:
-    #Y must be nx1 vector
-    #x must be the nxp designmatrix with all 1's in the first column 
+    #Y must be nx1 vector of the response
+    #x must be the nxp-1 vector of the predictors 
+
     def __init__(self):
       pass
-        
+
+    def designMatrix(self, X):
+        shape = np.shape(X)
+        design = np.ones((shape[0], shape[1]+1))
+        design[:,1:] = X
+        return design
     
     def Train(self, Y, x):
         self.Y = Y
         self.x = x
-        self.betaHat = np.linalg.inv(x.transpose() @ x) @ x.transpose() @ Y
-        self.Y_hat = x @ self.betaHat
+        self.dm = self.designMatrix(x)
+        self.betaHat = np.linalg.inv(self.dm.transpose() @ self.dm) @ self.dm.transpose() @ Y
+        self.Y_hat = self.dm @ self.betaHat
 
     def Predict(self, x):
-        return self.betaHat @ x
+        return self.betaHat @ self.designMatrix(x)
 
-    def mse(self):
-        mse = np.mean(np.square(self.Y_hat - self.Y))
-        return mse
+
 
 
 
--- test.py
+++ test.py
@@ -3,9 +3,8 @@ from sklearn import datasets
 import numpy as np
 
 X, y = datasets.load_diabetes(return_X_y=True)
-design = np.ones((442,11))
-design[:,1:] = X 
 
 lin = LinReg()
 lin.Train(y, X)
+print(lin.mse())
 
